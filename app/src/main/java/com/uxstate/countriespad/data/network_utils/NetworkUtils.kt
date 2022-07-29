53b8609ddd9fee0589e40c62f4b382761cc0b6e8
Tonnie <vontonnie@gmail.com>
2022-07-29 07:30:35 +0300

    Add Country Data Class under the domain package

--- app/src/main/java/com/uxstate/countriespad/data/network_utils/NetworkUtils.kt
+++ app/src/main/java/com/uxstate/countriespad/data/network_utils/NetworkUtils.kt
@@ -1,2 +1,6 @@
 package com.uxstate.countriespad.data.network_utils
 
+fun parseJsonString(string: String):List<Country> {
+
+
+}
---
+++ app/src/main/java/com/uxstate/countriespad/domain/model/Country.kt
@@ -0,0 +1,3 @@
+package com.uxstate.countriespad.domain.model
+
+data class Country(val name: String)
