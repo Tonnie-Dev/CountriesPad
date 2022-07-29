9254d9c899703b43e52debf4fadd146d912e7bc2
Tonnie <vontonnie@gmail.com>
2022-07-29 08:16:49 +0300

    Add JSONArray iterator extension function

--- app/src/main/java/com/uxstate/countriespad/data/network_utils/NetworkUtils.kt
+++ app/src/main/java/com/uxstate/countriespad/data/network_utils/NetworkUtils.kt
@@ -1,6 +1,34 @@
 package com.uxstate.countriespad.data.network_utils
 
-fun parseJsonString(string: String):List<Country> {
+import com.uxstate.countriespad.domain.model.Country
+import org.json.JSONArray
+
+
+
+fun parseJsonString(jsonString: String):List<Country> {
+
+    val numbers = arrayOf(1,2,3)
+
+    for(number in numbers){
+
+        println(number)
+    }
+
+    val countriesList = mutableListOf<Country>()
+    val countriesJsonArray = JSONArray(jsonString)
+
+    (0..countriesJsonArray.length()).forEach {  i ->
+
+        val country = countriesJsonArray.getJSONObject(i)
+    }
+
+    return countriesList
 
 
 }
+
+
+operator fun <T> JSONArray.iterator(): Iterator<T> =
+    (0 until this.length()).asSequence().map { this.get(it) as T }.iterator()
+
+
