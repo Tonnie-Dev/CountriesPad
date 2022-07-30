2ebd1f3719104557d16fdadf1bd303b5422be67f
Tonnie <vontonnie@gmail.com>
2022-07-30 14:36:47 +0300

    Add getCountriesData() dao function

--- app/src/main/java/com/uxstate/countriespad/data/local/CountryDAO.kt
+++ app/src/main/java/com/uxstate/countriespad/data/local/CountryDAO.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.uxstate.countriespad.domain.model.Country
 
 @Dao
 interface CountryDAO {
@@ -15,4 +16,16 @@ interface CountryDAO {
 
     @Query("DELETE  FROM countries_table ")
     suspend fun clearCountriesData()
+
+
+    @Query(
+            """
+        SELECT * FROM countries_table 
+WHERE 
+LOWER(name) LIKE '%' || LOWER (:query)|| '%'
+OR
+UPPER(:query)==ciocCode
+    """
+    )
+    suspend fun getCountriesData(query: String):List<Country>
 }
