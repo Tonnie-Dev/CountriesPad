8909f695b5541e783e7383cc5f88ee6e3ef32932
Tonnie <vontonnie@gmail.com>
2022-07-30 14:27:21 +0300

    Add insert dao function

--- app/src/main/java/com/uxstate/countriespad/data/local/CountryDAO.kt
+++ app/src/main/java/com/uxstate/countriespad/data/local/CountryDAO.kt
@@ -1,7 +1,14 @@
 package com.uxstate.countriespad.data.local
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 
 @Dao
 interface CountryDAO {
+
+    //insert query
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCountries()
 }
