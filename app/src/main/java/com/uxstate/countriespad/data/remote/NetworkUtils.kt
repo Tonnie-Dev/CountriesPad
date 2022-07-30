bdaa7af43ba5cac1a595b164464e23f3db99eefc
Tonnie <vontonnie@gmail.com>
2022-07-30 18:32:44 +0300

    Delete NetworkUtils class

--- app/src/main/java/com/uxstate/countriespad/data/remote/NetworkUtils.kt
+++
@@ -1,9 +0,0 @@
-package com.uxstate.countriespad.data.network_utils
-
-import com.uxstate.countriespad.domain.model.Country
-import org.json.JSONArray
-
-
-fun parseJsonString(jsonString: String): List<Country> {
-
-
