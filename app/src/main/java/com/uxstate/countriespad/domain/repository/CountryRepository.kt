b4147be2740f6b4660efacc8af52c82191fdadf9
Tonnie <vontonnie@gmail.com>
2022-07-30 14:43:24 +0300

    Add Repository defination interface functions

--- app/src/main/java/com/uxstate/countriespad/domain/repository/CountryRepository.kt
+++ app/src/main/java/com/uxstate/countriespad/domain/repository/CountryRepository.kt
@@ -1,4 +1,13 @@
 package com.uxstate.countriespad.domain.repository
 
-class CountryRepository {
+interface CountryRepository {
+
+    //delete country data
+    suspend fun clearCountriesData()
+
+    //insert country data
+    suspend fun insertCountriesData()
+    
+    //get country data
+    suspend fun getCountriesData(fetchFromRemote:Boolean)
 }
