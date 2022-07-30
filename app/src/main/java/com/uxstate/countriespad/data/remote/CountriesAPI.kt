2069c926be5b380caa3e6527b568ef0bd74f8ec8
Tonnie <vontonnie@gmail.com>
2022-07-30 13:36:00 +0300

    Add getCountriesJsonString API interface function

---
+++ app/src/main/java/com/uxstate/countriespad/data/remote/CountriesAPI.kt
@@ -0,0 +1,16 @@
+package com.uxstate.countriespad.data.remote
+
+import retrofit2.http.GET
+
+interface CountriesAPI {
+
+    @GET("v3.1/all")
+    fun getCountriesJsonString():String
+
+    companion object {
+
+        val BASE_URL = "https://restcountries.com/"
+    }
+}
+
+//https://restcountries.com/v3.1/all
