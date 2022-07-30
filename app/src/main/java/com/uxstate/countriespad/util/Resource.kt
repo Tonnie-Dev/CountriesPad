da1360082f13d1b0795b989c30f92f1e9b98882d
Tonnie <vontonnie@gmail.com>
2022-07-30 17:59:24 +0300

    Add Resource class in the Util package

---
+++ app/src/main/java/com/uxstate/countriespad/util/Resource.kt
@@ -0,0 +1,17 @@
+package com.uxstate.countriespad.util
+
+
+sealed class Resource<T>(val data: T? = null, val errorMessage: String? = null) {
+
+    /*We attach a nullable data to the Error case as we can
+     still return some data e.g. database cache*/
+    class Error<T>(data: T?= null, errorMessage: String) : Resource<T>(data, errorMessage)
+
+    class Success<T>(data: T?) : Resource<T>(data)
+
+    //it has a property which will be called from ViewModel
+    class Loading(val isLoading: Boolean) : Resource<T>()
+}
+
+
+
