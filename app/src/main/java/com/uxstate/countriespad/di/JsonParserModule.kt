d81ce8ffc06fe79a02ca30bea85b922fc586706f
Tonnie <vontonnie@gmail.com>
2022-07-31 14:09:05 +0300

    Add provideJSONStrinParser() di injection function

--- app/src/main/java/com/uxstate/countriespad/di/JsonParserModule.kt
+++ app/src/main/java/com/uxstate/countriespad/di/JsonParserModule.kt
@@ -1,5 +1,9 @@
 package com.uxstate.countriespad.di
 
+import com.uxstate.countriespad.data.json.CountriesListParser
+import com.uxstate.countriespad.data.json.JsonStringParser
+import com.uxstate.countriespad.domain.model.Country
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import javax.inject.Singleton
@@ -7,4 +11,13 @@ import javax.inject.Singleton
 @Module
 @InstallIn(Singleton::class)
 abstract class JsonParserModule {
+
+    //REPOSITORY
+    @Binds //@Binds used for 1-to-1 interface-implementation mapping
+    @Singleton
+
+    /*The abstract function takes only a single parameter which
+    is the interface implementation and the return type is the
+    interface implemented by the given parameter object.*/
+    abstract fun provideJSONStringParser(countriesListParser: CountriesListParser):JsonStringParser<Country>
 }
