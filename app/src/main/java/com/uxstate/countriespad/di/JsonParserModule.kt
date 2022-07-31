0a390a870f045bfa8430d3fcb01a97f112704b19
Tonnie <vontonnie@gmail.com>
2022-07-31 14:05:50 +0300

    Add JsonParser module abstract class

---
+++ app/src/main/java/com/uxstate/countriespad/di/JsonParserModule.kt
@@ -0,0 +1,10 @@
+package com.uxstate.countriespad.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import javax.inject.Singleton
+
+@Module
+@InstallIn(Singleton::class)
+abstract class JsonParserModule {
+}
