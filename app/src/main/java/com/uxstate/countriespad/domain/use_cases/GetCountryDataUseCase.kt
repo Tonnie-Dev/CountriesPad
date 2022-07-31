a2e27848f6ba32d48dc972785a9e12ae0de256e3
Tonnie <vontonnie@gmail.com>
2022-07-31 13:36:40 +0300

    Add invoke operator fun to GetCountryDataUseCase

--- app/src/main/java/com/uxstate/countriespad/domain/use_cases/GetCountryDataUseCase.kt
+++ app/src/main/java/com/uxstate/countriespad/domain/use_cases/GetCountryDataUseCase.kt
@@ -1,8 +1,17 @@
 package com.uxstate.countriespad.domain.use_cases
 
+import com.uxstate.countriespad.domain.model.Country
 import com.uxstate.countriespad.domain.repository.CountryRepository
+import com.uxstate.countriespad.util.Resource
+import kotlinx.coroutines.flow.Flow
 
 //always depend on abstractions
-class GetCountryDataUseCase (private val repository: CountryRepository){
+class GetCountryDataUseCase(private val repository: CountryRepository) {
+    
+    operator fun invoke(query: String, fetchFromRemote: Boolean): Flow<Resource<List<Country>>> {
+
+
+        return repository.getCountriesData(query, fetchFromRemote = fetchFromRemote)
+    }
 
 }
