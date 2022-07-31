739e9228eaf4556b43a1769b40f4e2593c8bcb1c
Tonnie <vontonnie@gmail.com>
2022-07-31 17:55:34 +0300

    Add Dimens data class for CompositionLocal

--- app/src/main/java/com/uxstate/countriespad/presentation/overview_screen/components/SearchBox.kt
+++ app/src/main/java/com/uxstate/countriespad/presentation/overview_screen/components/SearchBox.kt
@@ -1,18 +1,25 @@
 package com.uxstate.countriespad.presentation.overview_screen.components
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.TextFieldValue
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchBox(
     modifier: Modifier = Modifier,
     value: String,
     placeholderText: String,
-    onTextChange: () -> Unit
+    onQueryTextChange: (String) -> Unit
 ) {
 
 
-
-    TextFieldValue
+    OutlinedTextField(
+            modifier = modifier,
+            value = value,
+            onValueChange = onQueryTextChange,
+            singleLine = true,
+            label = {}
+    )
 }
