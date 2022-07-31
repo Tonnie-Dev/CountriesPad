4803a1e638311394201c33bfafadc66f29c1df0a
Tonnie <vontonnie@gmail.com>
2022-07-31 13:53:08 +0300

    Add provideCountryDatabase() di injection function

--- app/src/main/java/com/uxstate/countriespad/di/AppModule.kt
+++ app/src/main/java/com/uxstate/countriespad/di/AppModule.kt
@@ -1,5 +1,8 @@
 package com.uxstate.countriespad.di
 
+import android.app.Application
+import androidx.room.Room
+import com.uxstate.countriespad.data.local.CountryDatabase
 import com.uxstate.countriespad.data.remote.CountryAPI
 import dagger.Module
 import dagger.Provides
@@ -21,6 +24,16 @@ object AppModule {
                 .baseUrl(CountryAPI.BASE_URL)
                 .build()
                 .create()
+    }
+
+    @Provides
+    @Singleton
+
+    fun provideCountryDatabase(app: Application): CountryDatabase {
+
+        //doesn't include TypeConverters as we leave Room to do the initialization
+        return Room.databaseBuilder(app, CountryDatabase::class.java, "country_database")
+                .build()
     }
 
 }
