46c44b34185b587acbe33ac4daec070fc7d1a119
Tonnie <vontonnie@gmail.com>
2022-07-31 18:36:29 +0300

    Convert flagUrl to uri inside the CountryCard composable

--- app/src/main/java/com/uxstate/countriespad/presentation/overview_screen/components/CountryCard.kt
+++ app/src/main/java/com/uxstate/countriespad/presentation/overview_screen/components/CountryCard.kt
@@ -5,18 +5,25 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.core.net.toUri
 import com.uxstate.countriespad.domain.model.Country
 import com.uxstate.countriespad.util.LocalSpacing
 
 @Composable
-fun CountryCard(modifier:Modifier = Modifier, country: Country, onClickCountry: ()-> Unit) {
+fun CountryCard(modifier: Modifier = Modifier, country: Country, onClickCountry: () -> Unit) {
 
     val spacing = LocalSpacing.current
+    val flagUrl = country.flagUrl.toUri()
+            .buildUpon()
+            .scheme("https")
+            .build()
+
+    
     Card(
             modifier = modifier,
             colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceVariant),
             shape = MaterialTheme.shapes.large
-    ){
+    ) {
 
 
     }
