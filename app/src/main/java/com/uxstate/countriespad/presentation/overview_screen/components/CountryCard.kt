f3d95ed46de06054f883c01fd274f25e9e792e1d
Tonnie <vontonnie@gmail.com>
2022-07-31 18:39:04 +0300

    Add Image painter for CountryCard

--- app/src/main/java/com/uxstate/countriespad/presentation/overview_screen/components/CountryCard.kt
+++ app/src/main/java/com/uxstate/countriespad/presentation/overview_screen/components/CountryCard.kt
@@ -5,7 +5,11 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.core.net.toUri
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import com.uxstate.countriespad.R
 import com.uxstate.countriespad.domain.model.Country
 import com.uxstate.countriespad.util.LocalSpacing
 
@@ -18,7 +22,13 @@ fun CountryCard(modifier: Modifier = Modifier, country: Country, onClickCountry:
             .scheme("https")
             .build()
 
-    
+    val painter = rememberAsyncImagePainter(
+            model = ImageRequest.Builder(LocalContext.current)
+                    .data(flagUrl)
+                    .placeholder(
+                            R.drawable.loading_animation
+                    ).build()
+    )
     Card(
             modifier = modifier,
             colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceVariant),
