4a3c2f8a469c6bf8021e15d9db08c10dc17a0e43
Tonnie <vontonnie@gmail.com>
2022-07-31 18:34:47 +0300

    Add animated placeholder drawable

---
+++ app/src/main/java/com/uxstate/countriespad/presentation/overview_screen/components/CountryCard.kt
@@ -0,0 +1,23 @@
+package com.uxstate.countriespad.presentation.overview_screen.components
+
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.uxstate.countriespad.domain.model.Country
+import com.uxstate.countriespad.util.LocalSpacing
+
+@Composable
+fun CountryCard(modifier:Modifier = Modifier, country: Country, onClickCountry: ()-> Unit) {
+
+    val spacing = LocalSpacing.current
+    Card(
+            modifier = modifier,
+            colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceVariant),
+            shape = MaterialTheme.shapes.large
+    ){
+
+
+    }
+}
