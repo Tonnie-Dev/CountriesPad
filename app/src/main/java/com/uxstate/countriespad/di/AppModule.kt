90ca7dd25d5bbe3f8c8af1e0ff9ad2fc9d54eafc
Tonnie <vontonnie@gmail.com>
2022-07-31 13:48:50 +0300

    Add provideCountryAPI() di injection function

--- app/src/main/java/com/uxstate/countriespad/di/AppModule.kt
+++ app/src/main/java/com/uxstate/countriespad/di/AppModule.kt
@@ -1,7 +1,26 @@
 package com.uxstate.countriespad.di
 
+import com.uxstate.countriespad.data.remote.CountryAPI
 import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.create
+import javax.inject.Singleton
 
 @Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideCountryAPI(): CountryAPI {
+        //no converter as Retrofit is just returning raw JSON String
+        return Retrofit.Builder()
+                .baseUrl(CountryAPI.BASE_URL)
+                .build()
+                .create()
+    }
+
 }
