3cf1aa2fbb4bb04d054d336e6c1dee05d4db03b5
Tonnie <vontonnie@gmail.com>
2022-07-31 14:12:43 +0300

    Initialize Timber inside the CountryApp's onCreate()

--- app/src/main/java/com/uxstate/countriespad/CountryApp.kt
+++ app/src/main/java/com/uxstate/countriespad/CountryApp.kt
@@ -1,7 +1,14 @@
 package com.uxstate.countriespad
 
+import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class CountryApp {
+class CountryApp:Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+
 }
