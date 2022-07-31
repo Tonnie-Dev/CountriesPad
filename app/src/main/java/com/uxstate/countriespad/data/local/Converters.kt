c47df4a81416d7b5041268fc3bd0fef3adedc6da
Tonnie <vontonnie@gmail.com>
2022-07-31 12:02:45 +0300

    Add Converters helper class

---
+++ app/src/main/java/com/uxstate/countriespad/data/local/Converters.kt
@@ -0,0 +1,25 @@
+package com.uxstate.countriespad.data.local
+
+import androidx.room.ProvidedTypeConverter
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.lang.reflect.Type
+import java.util.*
+import kotlin.collections.ArrayList
+
+
+/*Be ware of using "," as separator as sometimes your string may
+have the same character and it can be a mess.*/
+
+
+
+
+class Converters {
+
+   
+
+}
+
+
+
