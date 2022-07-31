97cd345b3e2721eba6b5ccbf0b7da94cceb85d33
Tonnie <vontonnie@gmail.com>
2022-07-31 16:39:01 +0300

    Intialize state inside OverviewModel

--- app/src/main/java/com/uxstate/countriespad/presentation/overview_screen/OverviewViewModel.kt
+++ app/src/main/java/com/uxstate/countriespad/presentation/overview_screen/OverviewViewModel.kt
@@ -1,10 +1,19 @@
 package com.uxstate.countriespad.presentation.overview_screen
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.uxstate.countriespad.domain.use_cases.GetCountryDataUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class OverviewViewModel @Inject constructor(private val usecase:GetCountryDataUseCase) : ViewModel() {
+class OverviewViewModel @Inject constructor(private val useCase:GetCountryDataUseCase) : ViewModel() {
+
+
+    var state by mutableStateOf(OverviewState())
+    private set
+
+    
 }
