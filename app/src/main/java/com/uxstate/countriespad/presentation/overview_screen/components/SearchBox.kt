8aff176f0d5c91c674f8c645423f50e87551c705
Tonnie <vontonnie@gmail.com>
2022-07-31 17:26:54 +0300

    Upgrade md3 from alpha01 to alpha15

---
+++ app/src/main/java/com/uxstate/countriespad/presentation/overview_screen/components/SearchBox.kt
@@ -0,0 +1,18 @@
+package com.uxstate.countriespad.presentation.overview_screen.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+
+@Composable
+fun SearchBox(
+    modifier: Modifier = Modifier,
+    value: String,
+    placeholderText: String,
+    onTextChange: () -> Unit
+) {
+
+
+
+    TextFieldValue
+}
