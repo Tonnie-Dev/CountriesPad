35bf819b460dda983142a30886a690b3f21b213e
Tonnie <vontonnie@gmail.com>
2022-08-01 08:46:32 +0300

    Add LazyVertical Gridto OverviewScreen

--- app/src/main/java/com/uxstate/countriespad/presentation/overview_screen/OverviewScreen.kt
+++ app/src/main/java/com/uxstate/countriespad/presentation/overview_screen/OverviewScreen.kt
@@ -2,12 +2,19 @@ package com.uxstate.countriespad.presentation.overview_screen
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.uxstate.countriespad.presentation.overview_screen.components.SearchBox
+import com.uxstate.countriespad.R
+import com.uxstate.countriespad.presentation.overview_screen.components.CountryCard
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Destination(start = true)
@@ -19,15 +26,30 @@ fun OverviewScreen(
     viewModel: OverviewViewModel = hiltViewModel()
 ) {
 
+    val state = viewModel.state
+
 Scaffold(topBar = {
     LargeTopAppBar(  colors = TopAppBarDefaults.largeTopAppBarColors(
             containerColor = MaterialTheme.colorScheme.surfaceVariant,
-            titleContentColor = MaterialTheme.colorScheme.onSurfaceVariant), title = {}, actions = {} )
+            titleContentColor = MaterialTheme.colorScheme.onSurfaceVariant), title = {}, actions = {
+
+                SearchBox(value = state.query , placeholderText = stringResource(id = R.string.search_tag), onQueryTextChange = {} )
+    } )
 
 
 }){
     paddingValues ->
 
-    Text(text = "This is me", modifier = Modifier.padding(paddingValues))
+    LazyVerticalGrid(modifier = Modifier.padding(paddingValues), columns = GridCells.Fixed(2), content = {
+
+        items(state.countriesData){ country ->
+            CountryCard(country = country) {
+
+            }
+
+        }
+
+
+    })
 }
 }
