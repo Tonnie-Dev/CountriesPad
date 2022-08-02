c9bdeab279eed720fd50aad13b3247b3b5425091
Tonnie <vontonnie@gmail.com>
2022-08-02 08:02:59 +0300

    Add Odering events to OverviewEvent class

--- app/src/main/java/com/uxstate/countriespad/presentation/overview_screen/OverviewEvent.kt
+++ app/src/main/java/com/uxstate/countriespad/presentation/overview_screen/OverviewEvent.kt
@@ -1,10 +1,13 @@
 package com.uxstate.countriespad.presentation.overview_screen
 
 import com.uxstate.countriespad.domain.model.Country
+import com.uxstate.countriespad.util.CountryOrderFormat
 
 sealed class OverviewEvent (){
 
     data class OnQueryChange(val query:String):OverviewEvent()
     object OnClearSearchBox:OverviewEvent()
     data class OnClickCountry (val country: Country):OverviewEvent()
+    object toggleSelectionPane:OverviewEvent()
+    data class OnChangeOrder(val countryOrderFormat: CountryOrderFormat):OverviewEvent()
 }
