fb80894793773ba22408a5f1d067af7e689d22ae
Tonnie <vontonnie@gmail.com>
2022-08-02 07:13:11 +0300

    Add CountryOrderFormat subclasses

--- app/src/main/java/com/uxstate/countriespad/util/CountryOrderFormat.kt
+++ app/src/main/java/com/uxstate/countriespad/util/CountryOrderFormat.kt
@@ -1,3 +1,21 @@
 package com.uxstate.countriespad.util
 
-sealed class CountryOrderFormat
+import com.uxstate.countriespad.domain.model.Country
+
+sealed class CountryOrderFormat(val orderType: OrderType){
+
+    class ByName(orderType: OrderType):CountryOrderFormat(orderType)
+    class ByPopulation(orderType: OrderType):CountryOrderFormat(orderType)
+    class ByArea(orderType: OrderType):CountryOrderFormat(orderType)
+
+    fun copy(orderType: OrderType):CountryOrderFormat{
+
+
+        return when(this){
+
+            is ByName -> ByName(orderType)
+            is ByPopulation -> ByPopulation(orderType)
+            is ByArea -> ByArea(orderType)
+        }
+    }
+}
