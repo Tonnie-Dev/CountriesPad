c892937a6aae0673b43867fab17edfb70795367f
Tonnie <vontonnie@gmail.com>
2022-08-02 07:51:26 +0300

    Add mapCountryResponse util function

--- app/src/main/java/com/uxstate/countriespad/domain/use_cases/GetCountryDataUseCase.kt
+++ app/src/main/java/com/uxstate/countriespad/domain/use_cases/GetCountryDataUseCase.kt
@@ -6,6 +6,8 @@ import com.uxstate.countriespad.util.CountryOrderFormat
 import com.uxstate.countriespad.util.OrderType
 import com.uxstate.countriespad.util.Resource
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 
 //always depend on abstractions
 class GetCountryDataUseCase(
@@ -17,6 +19,59 @@ class GetCountryDataUseCase(
 
     operator fun invoke(query: String, fetchFromRemote: Boolean): Flow<Resource<List<Country>>> {
         return repository.getCountriesData(query, fetchFromRemote = fetchFromRemote)
+    }
+
+    fun mapCountryResponse(
+        response: Flow<Resource<List<Country>>>,
+        countryOrderFormat: CountryOrderFormat = CountryOrderFormat.ByName(
+                orderType = OrderType.Ascending
+        )
+    ): Flow<Resource<List<Country>>> = flow {
+
+response.map {
+    it.data
+
+}.map { countries ->
+    when(countryOrderFormat.orderType){
+
+        is OrderType.Ascending -> {
+
+            when(countryOrderFormat){
+
+                is CountryOrderFormat.ByName -> {
+
+                    emit(Resource.Success(countries?.sortedBy { it.name.lowercase() }))
+                }
+                is CountryOrderFormat.ByPopulation -> {
+                    emit(Resource.Success(countries?.sortedBy { it.population }))
+
+                }
+                is CountryOrderFormat.ByArea -> {
+                    emit(Resource.Success(countries?.sortedBy { it.area}))
+
+                }
+            }
+
+        }
+        is OrderType.Descending -> {
+            when(countryOrderFormat){
+
+                is CountryOrderFormat.ByName -> {
+                    emit(Resource.Success(countries?.sortedByDescending { it.name.lowercase() }))
+
+                }
+                is CountryOrderFormat.ByPopulation -> {
+
+                    emit(Resource.Success(countries?.sortedByDescending { it.population }))
+                }
+                is CountryOrderFormat.ByArea -> {
+
+                    emit(Resource.Success(countries?.sortedByDescending { it.area }))
+                }
+            }
+        }
+    }
+}
     }
 
 }
