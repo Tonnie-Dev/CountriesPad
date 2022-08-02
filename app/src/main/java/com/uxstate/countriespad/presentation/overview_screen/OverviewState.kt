3974e70f149d723a70c1fe8d051c04c1b3fde748
Tonnie <vontonnie@gmail.com>
2022-08-02 07:58:04 +0300

    Add extra states to OverviewState class

--- app/src/main/java/com/uxstate/countriespad/presentation/overview_screen/OverviewState.kt
+++ app/src/main/java/com/uxstate/countriespad/presentation/overview_screen/OverviewState.kt
@@ -1,10 +1,14 @@
 package com.uxstate.countriespad.presentation.overview_screen
 
 import com.uxstate.countriespad.domain.model.Country
+import com.uxstate.countriespad.util.CountryOrderFormat
+import com.uxstate.countriespad.util.OrderType
 
 data class OverviewState(
     val query: String = "",
     val countriesData: List<Country> = emptyList(),
     val isLoading: Boolean = false,
-val errorMessage:String = ""
+    val errorMessage: String = "",
+    val isOrderPaneVisible:Boolean= false,
+val countryOrderFormat: CountryOrderFormat = CountryOrderFormat.ByName(OrderType.Ascending)
 )
