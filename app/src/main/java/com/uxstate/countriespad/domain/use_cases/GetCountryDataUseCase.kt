0b4fceb57bb8266b5df3a3dba6088642bacae7d9
Tonnie <vontonnie@gmail.com>
2022-08-02 07:28:39 +0300

    Add countryOrderFormart parameter for GetCountryDataUseCase

--- app/src/main/java/com/uxstate/countriespad/domain/use_cases/GetCountryDataUseCase.kt
+++ app/src/main/java/com/uxstate/countriespad/domain/use_cases/GetCountryDataUseCase.kt
@@ -2,11 +2,18 @@ package com.uxstate.countriespad.domain.use_cases
 
 import com.uxstate.countriespad.domain.model.Country
 import com.uxstate.countriespad.domain.repository.CountryRepository
+import com.uxstate.countriespad.util.CountryOrderFormat
+import com.uxstate.countriespad.util.OrderType
 import com.uxstate.countriespad.util.Resource
 import kotlinx.coroutines.flow.Flow
 
 //always depend on abstractions
-class GetCountryDataUseCase(private val repository: CountryRepository) {
+class GetCountryDataUseCase(
+    private val repository: CountryRepository,
+    countryOrderFormat: CountryOrderFormat = CountryOrderFormat.ByName(
+            orderType = OrderType.Ascending
+    )
+) {
 
     operator fun invoke(query: String, fetchFromRemote: Boolean): Flow<Resource<List<Country>>> {
         return repository.getCountriesData(query, fetchFromRemote = fetchFromRemote)
