a08f0095da89cd2ff3e0f9c50fc066b4bf42d0d7
Tonnie <vontonnie@gmail.com>
2022-08-03 13:51:10 +0300

    Add FilterUseCase invoke function

--- app/src/main/java/com/uxstate/countriespad/domain/use_cases/FilterUseCase.kt
+++ app/src/main/java/com/uxstate/countriespad/domain/use_cases/FilterUseCase.kt
@@ -6,7 +6,55 @@ import com.uxstate.countriespad.util.OrderType
 
 class FilterUseCase {
 
-    operator fun invoke(list: List<Country>, orderFormat: CountryOrderFormat = CountryOrderFormat.ByName(OrderType.Descending)){
+    operator fun invoke(
+        countries: List<Country>,
+        orderFormat: CountryOrderFormat = CountryOrderFormat.ByName(OrderType.Ascending)
+    ): List<Country> {
+
+    return    when (orderFormat.orderType) {
+
+            //ASCENDING
+            is OrderType.Ascending -> {
+
+                when (orderFormat) {
+
+                    is CountryOrderFormat.ByName -> {
+
+                        countries.sortedBy { it.name }
+                    }
+                    is CountryOrderFormat.ByArea -> {
+
+                        countries.sortedBy { it.area }
+                    }
+                    is CountryOrderFormat.ByPopulation -> {
+
+                        countries.sortedBy { it.population }
+                    }
+                }
+
+            }
+
+            //DESCENDING
+            is OrderType.Descending -> {
+                when (orderFormat) {
+
+                    is CountryOrderFormat.ByName -> {
+
+                        countries.sortedByDescending { it.name }
+                    }
+                    is CountryOrderFormat.ByArea -> {
+
+                        countries.sortedByDescending { it.area }
+                    }
+                    is CountryOrderFormat.ByPopulation -> {
+
+                        countries.sortedByDescending { it.population }
+                    }
+                }
+
+            }
+        }
+
 
 
     }
