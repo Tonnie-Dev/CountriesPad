5f47ff0ef995ca517fd6115fb11f27b7d1760393
Tonnie <vontonnie@gmail.com>
2022-08-03 16:39:28 +0300

    Add DetailsScreen

---
+++ app/src/main/java/com/uxstate/countriespad/presentation/details_screen/DetailsScreen.kt
@@ -0,0 +1,10 @@
+package com.uxstate.countriespad.presentation.details_screen
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Destination()
+@Composable
+fun DetailsScreen() {
+
+}
