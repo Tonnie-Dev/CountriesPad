ad61d79f439d66748f2ee00d8b817cb59c530f70
Tonnie <vontonnie@gmail.com>
2022-08-04 21:21:06 +0300

    Add CoatOfArms composable components

--- app/src/main/java/com/uxstate/countriespad/presentation/details_screen/components/CoatOfArms.kt
+++ app/src/main/java/com/uxstate/countriespad/presentation/details_screen/components/CoatOfArms.kt
@@ -1,8 +1,55 @@
 package com.uxstate.countriespad.presentation.details_screen.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.core.net.toUri
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import com.uxstate.countriespad.R
+import com.uxstate.countriespad.domain.model.Country
+import com.uxstate.countriespad.util.LocalSpacing
 
 @Composable
-fun CoatOfArms() {
-    
+fun CoatOfArms(country: Country, modifier: Modifier = Modifier) {
+
+    val spacing = LocalSpacing.current
+    Row(modifier = modifier) {
+
+        val context = LocalContext.current
+        val imageUrl = country.coatOfArmsUrl.toUri()
+                .buildUpon()
+                .scheme("https")
+
+        val painter = rememberAsyncImagePainter(
+                model = ImageRequest.Builder(context)
+                        .data(imageUrl)
+                        .crossfade(true)
+                        .error(R.drawable.ic_empty_flag)
+                        .build()
+        )
+        Image(
+                painter = painter,
+                contentDescription = stringResource(id = R.string.coat_of_arms_label),
+                modifier = Modifier
+                        .fillMaxHeight()
+                        .aspectRatio(3f / 2f)
+        )
+
+        Spacer(modifier = Modifier.width(spacing.spaceSmall))
+        Column(
+                verticalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxHeight()
+        ) {
+            Text(text = country.name)
+            Text(text = country.ciocCode)
+
+        }
+
+    }
+
 }
