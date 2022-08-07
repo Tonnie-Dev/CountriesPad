11d9d0a7bc632bd1d6696395a1bb66c9502a15fe
Tonnie <vontonnie@gmail.com>
2022-08-07 17:11:56 +0300

    Add officialName property on Country data class

--- app/src/main/java/com/uxstate/countriespad/domain/model/Country.kt
+++ app/src/main/java/com/uxstate/countriespad/domain/model/Country.kt
@@ -6,6 +6,7 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class Country(
     val name: String,
+    val officialName:String,
     val currencies: List<String>,
     val capital: String,
     val region: String,
