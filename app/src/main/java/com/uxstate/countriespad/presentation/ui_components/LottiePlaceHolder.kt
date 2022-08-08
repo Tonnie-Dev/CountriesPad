feb24c8725e0727355797397b7f2ae998e8548ce
Tonnie <vontonnie@gmail.com>
2022-08-08 18:54:43 +0300

    Add LottiePlaceHolder composable

--- app/src/main/java/com/uxstate/countriespad/presentation/ui_components/LottiePlaceHolder.kt
+++ app/src/main/java/com/uxstate/countriespad/presentation/ui_components/LottiePlaceHolder.kt
@@ -1,20 +1,44 @@
 package com.uxstate.countriespad.presentation.ui_components
 
 import androidx.annotation.RawRes
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.rememberLottieComposition
+import com.airbnb.lottie.compose.*
 import com.uxstate.countriespad.R
+import com.uxstate.countriespad.util.LocalSpacing
 
 @Composable
-fun LottieAnimationDefination(modifier: Modifier = Modifier, @RawRes lottie:Int) {
+fun LottieAnimationDefinition(modifier: Modifier, @RawRes lottie: Int) {
 
+    val spacing = LocalSpacing.current
     //spec to hold anim
     val spec = LottieCompositionSpec.RawRes(R.raw.loading_lottie_anim)
 
     //composition to render a Lottie
 
-    val lottieComposition = rememberLottieComposition(spec = spec)
+    val lottieComposition by rememberLottieComposition(spec = spec)
+
+    //monitor state
+    val state by animateLottieCompositionAsState(
+            composition = lottieComposition,
+            iterations = LottieConstants.IterateForever
+    )
+
+    //lottie composable
+
+    LottieAnimation(
+            composition = lottieComposition,
+            progress = state,
+            modifier = Modifier.size(spacing.spaceOneHundredDp)
+    )
+
+}
+
+@Composable
+fun LottiePlaceHolder(modifier: Modifier, @RawRes lottie: Int) {
+
+    LottieAnimationDefinition(modifier = modifier, lottie = lottie)
 
 }
