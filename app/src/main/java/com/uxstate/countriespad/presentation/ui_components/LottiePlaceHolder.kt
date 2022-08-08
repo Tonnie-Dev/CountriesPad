86c6c8a836828829cca5f93681e86c7fb83a322f
Tonnie <vontonnie@gmail.com>
2022-08-08 18:45:13 +0300

    Add Lottie json animation file

--- app/src/main/java/com/uxstate/countriespad/presentation/ui_components/LottiePlaceHolder.kt
+++ app/src/main/java/com/uxstate/countriespad/presentation/ui_components/LottiePlaceHolder.kt
@@ -1,1 +1,20 @@
 package com.uxstate.countriespad.presentation.ui_components
+
+import androidx.annotation.RawRes
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.uxstate.countriespad.R
+
+@Composable
+fun LottieAnimationDefination(modifier: Modifier = Modifier, @RawRes lottie:Int) {
+
+    //spec to hold anim
+    val spec = LottieCompositionSpec.RawRes(R.raw.loading_lottie_anim)
+
+    //composition to render a Lottie
+
+    val lottieComposition = rememberLottieComposition(spec = spec)
+
+}
