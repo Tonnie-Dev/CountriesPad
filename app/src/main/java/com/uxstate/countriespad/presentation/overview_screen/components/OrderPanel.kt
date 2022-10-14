17d51f5052e3330127947d6500ec2849cae62752
Tonnie <vontonnie@gmail.com>
2022-10-14 08:53:02 +0300

    Wrap OrderPanel in a surface

--- app/src/main/java/com/uxstate/countriespad/presentation/overview_screen/components/OrderPanel.kt
+++ app/src/main/java/com/uxstate/countriespad/presentation/overview_screen/components/OrderPanel.kt
@@ -1,6 +1,9 @@
 package com.uxstate.countriespad.presentation.overview_screen.components
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -19,64 +22,73 @@ fun OrderPanel(
 ) {
 
 val spacing = LocalSpacing.current
-    Column(modifier = modifier) {
+   Surface(modifier = modifier,
+           color = MaterialTheme.colorScheme.surfaceVariant,
+           contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+           shadowElevation = spacing.spaceExtraSmall,
+           shape = RoundedCornerShape(spacing.spaceSmall)
+          )
 
-        //ROW_1
-        Row(modifier = modifier.fillMaxWidth()) {
-            //R1
-            CustomRadioButton(text = stringResource(id = R.string.name_label),
-                    selected = countryOrder is CountryOrderFormat.ByName,
-                    onSelected = { onOrderChange(CountryOrderFormat.ByName(countryOrder.orderType)) })
+           {
+        Column(modifier = Modifier.padding(spacing.spaceSmall)) {
+
+            //ROW_1
+            Row(modifier = modifier.fillMaxWidth()) {
+                //R1
+                CustomRadioButton(text = stringResource(id = R.string.name_label),
+                        selected = countryOrder is CountryOrderFormat.ByName,
+                        onSelected = { onOrderChange(CountryOrderFormat.ByName(countryOrder.orderType)) })
 
 
 
-            Spacer(modifier = Modifier.width(spacing.spaceSmall))
+                Spacer(modifier = Modifier.width(spacing.spaceSmall))
 
-            //R2
+                //R2
 
-            CustomRadioButton(
+                CustomRadioButton(
 
-                    text = stringResource(id = R.string.area_label),
-                    selected = countryOrder is CountryOrderFormat.ByArea,
+                        text = stringResource(id = R.string.area_label),
+                        selected = countryOrder is CountryOrderFormat.ByArea,
 
-                    onSelected = { onOrderChange(CountryOrderFormat.ByArea(countryOrder.orderType)) }
-            )
+                        onSelected = { onOrderChange(CountryOrderFormat.ByArea(countryOrder.orderType)) }
+                )
 
-            Spacer(modifier = Modifier.width(spacing.spaceSmall))
+                Spacer(modifier = Modifier.width(spacing.spaceSmall))
 
-            //R3
+                //R3
 
-            CustomRadioButton(
-                    text = stringResource(id = R.string.population_label),
-                    selected = countryOrder is CountryOrderFormat.ByPopulation,
-                    onSelected = { onOrderChange(CountryOrderFormat.ByPopulation(countryOrder.orderType)) }
-            )
+                CustomRadioButton(
+                        text = stringResource(id = R.string.population_label),
+                        selected = countryOrder is CountryOrderFormat.ByPopulation,
+                        onSelected = { onOrderChange(CountryOrderFormat.ByPopulation(countryOrder.orderType)) }
+                )
+
+            }
+
+            Spacer(modifier = Modifier.height(spacing.spaceExtraSmall))
+
+            //ROW_2
+            Row(modifier = modifier.fillMaxWidth()) {
+
+                //R4
+
+                CustomRadioButton(
+                        text = "Ascending",
+                        selected = countryOrder.orderType is OrderType.Ascending,
+                        onSelected = { onOrderChange(countryOrder.copy(OrderType.Ascending)) }
+                )
+                Spacer(modifier = Modifier.width(spacing.spaceSmall))
+
+                //R5
+
+                CustomRadioButton(
+                        text = "Descending",
+                        selected = countryOrder.orderType is OrderType.Descending,
+                        onSelected = { onOrderChange(countryOrder.copy(OrderType.Descending)) }
+                )
+            }
+
 
         }
-
-        Spacer(modifier = Modifier.height(spacing.spaceExtraSmall))
-
-        //ROW_2
-        Row(modifier = modifier.fillMaxWidth()) {
-
-            //R4
-
-            CustomRadioButton(
-                    text = "Ascending",
-                    selected = countryOrder.orderType is OrderType.Ascending,
-                    onSelected = { onOrderChange(countryOrder.copy(OrderType.Ascending)) }
-            )
-            Spacer(modifier = Modifier.width(spacing.spaceSmall))
-
-            //R5
-
-            CustomRadioButton(
-                    text = "Descending",
-                    selected = countryOrder.orderType is OrderType.Descending,
-                    onSelected = { onOrderChange(countryOrder.copy(OrderType.Descending)) }
-            )
-        }
-
-
     }
 }
