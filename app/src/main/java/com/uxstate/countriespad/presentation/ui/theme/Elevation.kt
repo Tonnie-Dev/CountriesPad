4eb5d8c5618c3adf1ccb2691c7149dba002ee152
Tonnie <vontonnie@gmail.com>
2023-08-25 08:25:26 +0300

    Add Elevation File

---
+++ app/src/main/java/com/uxstate/countriespad/presentation/ui/theme/Elevation.kt
@@ -0,0 +1,16 @@
+package com.uxstate.countriespad.presentation.ui.theme
+
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+data class Elevation (val level0:Dp = 0.0.dp,
+                      val level1:Dp = 1.0.dp,
+                      val level2:Dp = 3.0.dp,
+                      val level3:Dp = 6.0.dp,
+                      val level4:Dp = 8.0.dp,
+                      val level5:Dp = 12.0.dp)
+
+
+
+val LocalElevation = compositionLocalOf {  Elevation()}
