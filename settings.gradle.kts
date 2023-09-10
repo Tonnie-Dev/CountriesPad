ec37240167858c9cb5ff94f19d7c2417d54d4147
Tonnie <vontonnie@gmail.com>
2023-09-10 09:55:04 +0300

    Upgrade AGP to 8.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,16 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+
+    plugins {
+        id("de.fayard.refreshVersions") version "0.60.2"
+    }
 }
+
+plugins {
+    id("de.fayard.refreshVersions")
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
