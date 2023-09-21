93f9e2f0e2b9c8a17a075ffe5c1dae8014b8fca1
Tonnie <vontonnie@gmail.com>
2023-09-21 07:44:42 +0300

    Improve Navigation Bar Code

--- app/src/main/java/com/uxstate/countriespad/navigation/CoreNavigator.kt
+++ app/src/main/java/com/uxstate/countriespad/navigation/CoreNavigator.kt
@@ -2,38 +2,36 @@ package com.uxstate.countriespad.navigation
 
 import androidx.navigation.NavController
 import com.ramcosta.composedestinations.navigation.navigate
+import com.uxstate.overview.presentation.destinations.DetailsScreenDestination
+import com.uxstate.overview.presentation.destinations.OverviewScreenDestination
 import com.uxstate.overview.presentation.home_screen.OverviewScreenNavigator
+import com.uxstate.stats.destinations.StatsScreenDestination
 import com.uxstate.util.model.Country
 import com.uxstate.validator.destinations.ValidatorScreenDestination
 
 
-class CoreNavigator(private val navController: NavController) : OverviewScreenNavigator{
+class CoreNavigator(private val navController: NavController) : OverviewScreenNavigator {
     override fun navigateBackToOverviewScreen() {
-//navController.navigate(OverviewScreenDestination)
-
+        navController.navigate(OverviewScreenDestination)
 
     }
-
 
 
     override fun navigateToValidator() {
         navController.navigate(ValidatorScreenDestination)
-
-
     }
-
 
 
     override fun navigateToDetailsScreen(country: Country) {
-        //navController.navigate(DetailsScreenDestination(country))
+        navController.navigate(DetailsScreenDestination(country))
     }
 
     override fun navigateToValidatorScreen() {
-        TODO("Not yet implemented")
+        navController.navigate(ValidatorScreenDestination)
     }
 
     override fun navigateToStatsScreen() {
-        TODO("Not yet implemented")
+        navController.navigate(StatsScreenDestination)
     }
 
 
--- feature/overview/src/main/java/com/uxstate/overview/presentation/home_screen/components/BottomNavigationBar.kt
+++ feature/overview/src/main/java/com/uxstate/overview/presentation/home_screen/components/BottomNavigationBar.kt
@@ -11,7 +11,13 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
@@ -22,12 +28,13 @@ import com.uxstate.ui.R
 @Composable
 fun BottomNavigationBar(
     navigator: OverviewScreenNavigator,
-    selectedIndex: Int,
-    onDestinationChange:(navIndex:Int)-> Unit
+    onDestinationChange: (navIndex: Int) -> Unit
 ) {
+    var selectedItemIndex by rememberSaveable { mutableIntStateOf(0) }
 
     val containerColor = MaterialTheme.colorScheme.surfaceContainer
-    val contentColor: Color = MaterialTheme.colorScheme.onSurface
+    //val containerColor = NavigationBarDefaults.containerColor
+    val contentColor: Color = MaterialTheme.colorScheme.contentColorFor(containerColor)
 
     val items = listOf(
             BottomNavigationItem(
@@ -51,18 +58,30 @@ fun BottomNavigationBar(
 
         items.forEachIndexed { index, item ->
 
+            val isSelected = selectedItemIndex == index
             NavigationBarItem(
-                    selected = selectedIndex == index,
+                    selected = selectedItemIndex == index,
                     onClick = {
-                        when (selectedIndex) {
+                        selectedItemIndex = index
 
-                            1 -> navigator.navigateToValidatorScreen()
-                        }
+                        //  onDestinationChange(selectedItemIndex)
+                        /* when (selectedItemIndex) {
+
+                             1 -> onDestinationChange(1)
+                             2 -> onDestinationChange(2)
+                         }*/
                     },
                     icon = {
-                       Icon(
-                                imageVector = if (selectedIndex == index) item.selectedIcon else item.unSelectedIcon,
-                                contentDescription = item.title
+                        Icon(
+                                imageVector = if (isSelected) item.selectedIcon else item.unSelectedIcon,
+                                contentDescription = item.title,
+                                tint = if (isSelected) MaterialTheme.colorScheme.primary else contentColor
+                        )
+                    },
+                    label = {
+                        Text(
+                                text = item.title,
+                                color = if (isSelected) MaterialTheme.colorScheme.primary else contentColor
                         )
                     }
             )
@@ -76,6 +95,7 @@ data class BottomNavigationItem(
     val title: String,
     val selectedIcon: ImageVector,
     val unSelectedIcon: ImageVector
+
 )
 
 
