8f693caa3d7a1087822310dbd744213227fb75ae
Tonnie <vontonnie@gmail.com>
2023-12-10 14:58:38 +0300

    Comment on SignConfig line

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ android {
                     getDefaultProguardFile("proguard-android-optimize.txt"),
                     "proguard-rules.pro"
             )
-            signingConfig = signingConfigs.getByName("debug")
+            //signingConfig = signingConfigs.getByName("debug")
 
 
         }
