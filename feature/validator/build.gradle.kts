7e96b4a8c74cdfb4510b7513134ac31a5ab9f470
Tonnie <vontonnie@gmail.com>
2025-08-31 19:25:50 +0300

    Downgrade Java and Kotlin target to version 11

--- feature/validator/build.gradle.kts
+++ feature/validator/build.gradle.kts
@@ -29,13 +29,14 @@ android {
         targetCompatibility= JavaVersion.VERSION_18
 
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
     kotlinOptions {
-        jvmTarget = "18"
+        jvmTarget = "11"
     }
 
-    buildFeatures {
-        compose = true
-    }
     composeOptions {
         kotlinCompilerExtensionVersion = ProjectConfig.kotlinCompilerExtensionVersion
     }
